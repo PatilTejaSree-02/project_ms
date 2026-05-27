4e40f1d68ac46a628e7552350b6ff9c9b807c49d
PatilTejaSree-02 <patiltejasree02@gmail.com>
2026-05-27 18:41:13 +0530

    Complete infrastructure foundation phase

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function App() {
+
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/api/health")
+      .then((response) => {
+        setMessage(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
-    <div className="h-screen bg-black text-white flex items-center justify-center text-5xl font-bold">
-      Smart Meeting Hub
+    <div className="h-screen bg-black text-white flex items-center justify-center text-3xl font-bold">
+      {message}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
